73d3a78f9fb2c7d558d4c98e8f99f12ac01a14e3
KhanVl <vovahan2002@gmail.com>
2025-12-09 21:17:11 +0900

    fix bugs

--- src/api/axios/axios.ts
+++ src/api/axios/axios.ts
@@ -2,5 +2,16 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://utown-api.habsida.net/api",
-  withCredentials: true,
+  withCredentials: false,
+});
+
+api.interceptors.request.use((config) => {
+  const token = sessionStorage.getItem("token");
+
+  if (token) {
+    config.headers = config.headers ?? {};
+    (config.headers as any).Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
